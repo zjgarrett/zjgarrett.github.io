9e1d64e4d054643b0919c438b2b65e287d8b0adf
zjgarrett <garre160@msu.edu>
2023-12-25 14:33:06 -0500

    Added changing background of scroll

--- personal-page-app/src/App.js
+++ personal-page-app/src/App.js
@@ -1,3 +1,5 @@
+import React, {useEffect, useRef} from 'react';
+
 import logo from './logo.svg';
 import './styles/App.css';
 
@@ -6,16 +8,53 @@ import Header from "./components/Header.js";
 import Home from "./components/Home.js";
 
 function App() {
+ const canvasRef = useRef();
+
+ useEffect(() => {
+  const root = document.getElementById("root");
+  const updateBackground = () => {
+    const c = canvasRef.current;
+    const ctx = c.getContext("2d");
+
+    const scrollDistance = root.scrollTop || document.documentElement.scrollTop;
+    const maxDistance = root.scrollHeight - root.clientHeight;
+    const percentScrolled = Math.min(Math.log2(1 + 4* scrollDistance / maxDistance), 1);
+
+    const percentNotScrolled = 1 - percentScrolled;
+    const secondHeight = Math.max(percentNotScrolled, .1);
+
+    const grd = ctx.createLinearGradient(0, 0, 0, c.height);
+    grd.addColorStop(0, "#61dafb");
+    grd.addColorStop(1 * secondHeight, "#d9e8ff");
+    grd.addColorStop(1, "white");
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, c.width, c.height);
+  };
+
+  // Call updateBackground once the document is loaded
+  updateBackground();
+  // Call updateBackground on scroll
+  root.addEventListener("scroll", updateBackground);
+
+  // Cleanup function
+  return () => {
+    root.removeEventListener("scroll", updateBackground);
+  };
+}, []); // Empty dependency array means this effect runs once after initial render
+
   return (
     <Router>
-      <Header />
-      <body>
+      <canvas ref={canvasRef} id="background" width="100" height="100"></canvas>
+      <div id="container">
+        <Header />
         <div id="MainContent">
           <Routes>
             <Route element={ <Home/> }  path=''/>
           </Routes>
         </div>
-      </body>
+      </div>
     </Router>
   );
 }
